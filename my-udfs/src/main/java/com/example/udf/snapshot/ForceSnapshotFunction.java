9992530e842eb28ec2bfa476409b07dc85251c9c
Dave Snyder <daverted@gmail.com>
2019-01-03 12:28:29 -0700

    remove comment

--- my-udfs/src/main/java/com/example/udf/snapshot/ForceSnapshotFunction.java
+++ my-udfs/src/main/java/com/example/udf/snapshot/ForceSnapshotFunction.java
@@ -92,7 +92,6 @@ public class ForceSnapshotFunction {
         try {
           Thread.sleep(5000);
         } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
           e.printStackTrace();
         }
       }
