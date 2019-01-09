5c453ae7c6d217c1fec1c445844e3196902998cd
Dave Snyder <daverted@gmail.com>
2019-01-09 15:57:43 -0700

    change test view

---
+++ my-udfs/src/main/java/com/example/udf/helloworld/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.example.udf.helloworld;
+
+import com.takipi.api.client.ApiClient;
+import com.takipi.api.client.request.event.EventRequest;
+import com.takipi.api.client.result.event.EventResult;
+import com.takipi.api.core.url.UrlClient.Response;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    EventRequest eventRequest = EventRequest.newBuilder()
+      .setServiceId("S37295")
+      .setEventId("119")
+      .build();
+
+    ApiClient apiClient = ApiClient.newBuilder()
+      .setHostname("https://api.overops.com")
+      .setApiKey("***REMOVED***")
+      .build();
+
+    // GET event data
+    Response<EventResult> eventResponse = apiClient.get(eventRequest);
+
+    // check for a bad API response (HTTP status code >= 300)
+    if (eventResponse.isBadResponse())
+      throw new IllegalStateException("Failed getting events.");
+
+    // EventResult is a POJO: /blob/master/api-client/src/main/java/com/takipi/api/client/result/event/EventResult.java
+    EventResult eventResult = eventResponse.data;
+    
+    System.out.println("ID: " + eventResult.id);
+    System.out.println("Introduced by: " + eventResult.introduced_by);
+    System.out.println("Name " + eventResult.name);
+    System.out.println("Message: " + eventResult.message);
+    
+  }
+}
---
+++ my-udfs/src/main/java/com/example/udf/helloworld/HelloWorld4.java
@@ -0,0 +1,35 @@
+package com.example.udf.helloworld;
+
+import com.takipi.api.client.ApiClient;
+import com.takipi.api.client.data.view.SummarizedView;
+import com.takipi.api.client.request.view.ViewsRequest;
+import com.takipi.api.client.result.view.ViewsResult;
+import com.takipi.api.core.url.UrlClient.Response;
+
+public class HelloWorld4 {
+
+	public static void main(String[] args) {
+
+		String serviceId = "S37295";
+
+		ApiClient apiClient = ApiClient.newBuilder().setHostname("https://api.overops.com")
+				.setApiKey("***REMOVED***").build();
+
+		// get all views
+		ViewsRequest viewsRequest = ViewsRequest.newBuilder().setServiceId(serviceId).build();
+
+		// GET view data
+		Response<ViewsResult> viewsResponse = apiClient.get(viewsRequest);
+
+		// check for a bad API response (HTTP status code >= 300)
+		if (viewsResponse.isBadResponse())
+			throw new IllegalStateException("Failed getting views.");
+
+		ViewsResult viewsResult = viewsResponse.data;
+
+	    for(SummarizedView view : viewsResult.views) {
+	    	System.out.println(view.name + "(" + view.id + ")");
+	    }
+
+	}
+}
