0fc4f29c021165eb185012df94266bf61079795e
Dave Snyder <daverted@gmail.com>
2019-05-10 09:19:55 -0600

    debugging

--- jira-integration/src/main/java/com/overops/udf/jira/JiraIntegrationFunction.java
+++ jira-integration/src/main/java/com/overops/udf/jira/JiraIntegrationFunction.java
@@ -1,26 +1,19 @@
 package com.overops.udf.jira;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.URI;
-
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.time.*;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.Set;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
@@ -58,19 +51,9 @@ public class JiraIntegrationFunction {
 	}
 
 	public static void execute(String rawContextArgs, String rawInput) {
-		Runtime runtime = Runtime.getRuntime();
-		String freeMemory = runtime.freeMemory() + ""; // quick convert long to String
-	  String totalMemory = runtime.totalMemory() + "";
-
 		JiraIntegrationInput input = getJiraIntegrationInput(rawInput);
 
 		ContextArgs args = (new Gson()).fromJson(rawContextArgs, ContextArgs.class);
-
-		System.out.println("current time: " + LocalTime.now());
-		System.out.println("execute context: " + rawContextArgs);
-		System.out.println("free memory: " + freeMemory);
-		System.out.println("total memory: " + totalMemory);
-
 
 		if (!args.validate()) {
 			System.out.println("Bad context args");
@@ -89,12 +72,21 @@ public class JiraIntegrationFunction {
 
 		try {
 
+			ArrayList<Long> jiraTimer = new ArrayList<Long>();
+			ArrayList<Long> overopsTimer = new ArrayList<Long>();
+
+			Instant start = Instant.now(); // timer
+
 			uri = new URI(input.jiraURL);
 			JiraRestClient client = factory.createWithBasicHttpAuthentication(uri, input.jiraUsername, input.jiraPassword);
 
 			// Invoke the JRJC Client
 			Promise<User> promise = client.getUserClient().getUser(input.jiraUsername);
 			User user = promise.claim();
+
+			Instant finish = Instant.now(); // timer
+			long timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+			jiraTimer.add(timeElapsed);
 
 			// Sanity check
 			System.out.println(String.format("Your Jira user's email address is: %s\r\n", user.getEmailAddress()));
@@ -113,8 +105,15 @@ public class JiraIntegrationFunction {
 		EventsRequest eventsRequest = EventsRequest.newBuilder().setServiceId(args.serviceId).setViewId(args.viewId)
 			.setFrom(from.toString(fmt)).setTo(to.toString(fmt)).build();
 
+		start = Instant.now(); // timer
+
 		// GET events
 		Response<EventsResult> eventsResponse = apiClient.get(eventsRequest);
+
+		finish = Instant.now(); // timer
+		timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+		overopsTimer.add(timeElapsed);
+
 
 		// validate API response
 		if (eventsResponse.isBadResponse()) {
@@ -136,31 +135,51 @@ public class JiraIntegrationFunction {
 
 		System.out.println(events.size() + " events");
 
+		Set<String> callStackGroupSet = new HashSet<String>();
+
 			// for each event with a Jira issue URL
 			for (EventResult event : events) {
 				if (event.jira_issue_url != null) {
 
+					// ignore similar events
+					if (callStackGroupSet.contains(event.call_stack_group)) {
+						System.out.println("IGNORE EVENT: " + event);
+						continue;
+					} else {
+						callStackGroupSet.add(event.call_stack_group);
+					}
+
 					// get Jira issue ID from OO event's Jira issue URL
 					String issueId = getJiraIssueId(event.jira_issue_url);
-					System.out.println(issueId + " >> " + event.toString());
+					System.out.print(issueId + " >> " + event.toString());
 
 					// get issue from Jira
+					start = Instant.now(); // timer
+
 					Issue issue = client.getIssueClient().getIssue(issueId).claim();
+
+					finish = Instant.now(); // timer
+					timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+					jiraTimer.add(timeElapsed);
 
 					// get Jira issue status
 					String issueStatus = issue.getStatus().getName();
-					System.out.println("issue status: " + issueStatus);
+					System.out.print(" issue status: " + issueStatus);
 
 					// Jira updated date
 					DateTime jiraDate = issue.getUpdateDate();
-					System.out.println("jira updated date: " + jiraDate.toString());
+					System.out.print(" jira updated date: " + jiraDate.toString());
+
+					start = Instant.now();
 
 					// OverOps updated date
 					DateTime overopsDate = getUpdateDate(args.serviceId, event.id, apiClient);
-					System.out.println("overops updated date: " + jiraDate.toString());
+					finish = Instant.now(); // timer
+					timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+					overopsTimer.add(timeElapsed);
 
-					// TODO open a jira ticket for an API that lets me query for Jira / open / hidden ... to
-					// not time sync
+					System.out.print(" overops updated date: " + overopsDate.toString());
+
 					// TODO resurfaced = deployment version needs to change <---
 
 					// TODO ensure Jira statuses are unique
@@ -177,78 +196,38 @@ public class JiraIntegrationFunction {
 					boolean isResurfacedStatus = issueStatus.equals(input.resurfacedStatus); // is resurfaced in Jira?
 
 					// compare dates
-					if (jiraDate.isAfter(overopsDate)) {
-						// Sync Jira to OverOps
-						System.out.println("Sync Jira >> OverOps");
-
-						if (isResolvedStatus) {
-							// 1. resolve in Jira → resolve in OO
-							System.out.println("Resolved in Jira");
-
-							// if not resolved, resolve in OverOps
-							if (!isResolved) {
-								System.out.println("NOT Resolved in OverOps. Resolving...");
-								EventMarkResolvedRequest resolvedRequest = EventMarkResolvedRequest.newBuilder()
-										.setServiceId(args.serviceId).setEventId(event.id).build();
-
-								Response<EmptyResult> resolvedResponse = apiClient.post(resolvedRequest);
-
-								if (resolvedResponse.isBadResponse())
-									throw new IllegalStateException("Failed resolving event " + event.id);
-							}
-
-						} else if (isHiddenStatus) {
-							// 2. hide in Jira → hide in OO
-							System.out.println("Hidden in Jira");
-
-							// if not hidden, hide (delete/trash/archive) in OverOps
-							if (!isHidden) {
-								System.out.println("NOT Hidden in OverOps. Hiding...");
-								EventDeleteRequest hideRequest = EventDeleteRequest.newBuilder().setServiceId(args.serviceId)
-										.setEventId(event.id).build();
-
-								Response<EmptyResult> hideResponse = apiClient.post(hideRequest);
-
-								if (hideResponse.isBadResponse())
-									throw new IllegalStateException("Failed hiding event " + event.id);
-							}
-
-						} else {
-							// 3. anything else in Jira → move to inbox in OO
-							System.out.println("OTHER in Jira");
-
-							// if not in inbox, move to inbox in OverOps
-							if (!isInbox) {
-								System.out.println("NOT in Inbox in OverOps. Moving to Inbox...");
-								EventInboxRequest inboxRequest = EventInboxRequest.newBuilder().setServiceId(args.serviceId)
-										.setEventId(event.id).build();
-
-								Response<EmptyResult> resolvedResponse = apiClient.post(inboxRequest);
-
-								if (resolvedResponse.isBadResponse())
-									throw new IllegalStateException("Failed moving event to inbox " + event.id);
-							}
-
-						}
-
-					} else {
+					if (overopsDate.isAfter(jiraDate)) {
 						// Sync OverOps to Jira
-						System.out.println("Sync OverOps >> Jira");
+						System.out.print(" Sync OverOps >> Jira");
+
+						start = Instant.now();
 
 						// get possible status transitions
 						Iterable<Transition> transitions = client.getIssueClient().getTransitions(issue).claim();
 
+						finish = Instant.now(); // timer
+						timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+						jiraTimer.add(timeElapsed);
+
 						if (isResolved) {
 							// 1. resolve in OO → resolve in Jira
-							System.out.println("Resolved in OverOps");
+							System.out.print(" Resolved in OverOps");
 
 							// if not resolved, resolve in Jira
 							if (!isResolvedStatus) {
-								System.out.println("NOT Resolved in Jira. Resolving...");
+								System.out.print(" NOT Resolved in Jira. Resolving...");
 								for (Transition transition : transitions) {
 									if (transition.getName().equals(input.resolvedStatus)) {
 										TransitionInput transitionInput = new TransitionInput(transition.getId());
+
+										start = Instant.now(); // timer
+
 										client.getIssueClient().transition(issue, transitionInput).claim();
+
+										finish = Instant.now(); // timer
+										timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+										jiraTimer.add(timeElapsed);
+
 										break;
 									}
 								}
@@ -256,15 +235,23 @@ public class JiraIntegrationFunction {
 
 						} else if (isHidden) {
 							// 2. hide in OO → hide in Jira
-							System.out.println("Hidden in OverOps");
+							System.out.print(" Hidden in OverOps");
 
 							// if not hidden, hide in Jira
 							if (!isHiddenStatus) {
-								System.out.println("NOT Hidden in Jira. Hiding...");
+								System.out.print(" NOT Hidden in Jira. Hiding...");
 								for (Transition transition : transitions) {
 									if (transition.getName().equals(input.hiddenStatus)) {
 										TransitionInput transitionInput = new TransitionInput(transition.getId());
+
+										start = Instant.now(); // timer
+
 										client.getIssueClient().transition(issue, transitionInput).claim();
+
+										finish = Instant.now(); // timer
+										timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+										jiraTimer.add(timeElapsed);
+
 										break;
 									}
 								}
@@ -272,14 +259,22 @@ public class JiraIntegrationFunction {
 
 						} else if (isResurfaced) {
 							// 3. resurfaced in OO → resurfaced in Jira
-							System.out.println("Resurfaced in OverOps");
+							System.out.print(" Resurfaced in OverOps");
 
 							if (!isResurfacedStatus) {
-								System.out.println("NOT Resurfaced in Jira. Resurfacing...");
+								System.out.print(" NOT Resurfaced in Jira. Resurfacing...");
 								for (Transition transition : transitions) {
 									if (transition.getName().equals(input.resurfacedStatus)) {
 										TransitionInput transitionInput = new TransitionInput(transition.getId());
+
+										start = Instant.now();
+
 										client.getIssueClient().transition(issue, transitionInput).claim();
+
+										finish = Instant.now(); // timer
+										timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+										jiraTimer.add(timeElapsed);
+
 										break;
 									}
 								}
@@ -287,18 +282,99 @@ public class JiraIntegrationFunction {
 
 						} else {
 							// 4. anything else, mark "in Inbox" in Jira
-							System.out.println("OTHER in OverOps");
+							System.out.print(" OTHER in OverOps");
 
 							if (!isInboxStatus) {
-								System.out.println("NOT in Inbox in Jira. Moving to Inbox...");
+								System.out.print(" NOT in Inbox in Jira. Moving to Inbox...");
 								for (Transition transition : transitions) {
 									if (transition.getName().equals(input.inboxStatus)) {
 										TransitionInput transitionInput = new TransitionInput(transition.getId());
+
+										start = Instant.now(); // timer
+
 										client.getIssueClient().transition(issue, transitionInput).claim();
+
+										finish = Instant.now(); // timer
+										timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+										jiraTimer.add(timeElapsed);
+
 										break;
 									}
 								}
 							}
+						}
+
+					} else {
+						// Sync Jira to OverOps
+						System.out.print(" [Sync Jira >> OverOps] ");
+
+						if (isResolvedStatus) {
+							// 1. resolve in Jira → resolve in OO
+							System.out.print(" Resolved in Jira");
+
+							// if not resolved, resolve in OverOps
+							if (!isResolved) {
+								System.out.print(" NOT Resolved in OverOps. Resolving...");
+								EventMarkResolvedRequest resolvedRequest = EventMarkResolvedRequest.newBuilder()
+										.setServiceId(args.serviceId).setEventId(event.id).setHandleSimilarEvents(true).build();
+
+								start = Instant.now(); // timer
+
+								Response<EmptyResult> resolvedResponse = apiClient.post(resolvedRequest);
+
+								finish = Instant.now(); // timer
+								timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+								overopsTimer.add(timeElapsed);
+
+
+								if (resolvedResponse.isBadResponse())
+									throw new IllegalStateException("Failed resolving event " + event.id);
+							}
+
+						} else if (isHiddenStatus) {
+							// 2. hide in Jira → hide in OO
+							System.out.print(" Hidden in Jira");
+
+							// if not hidden, hide (delete/trash/archive) in OverOps
+							if (!isHidden) {
+								System.out.print(" NOT Hidden in OverOps. Hiding...");
+								EventDeleteRequest hideRequest = EventDeleteRequest.newBuilder().setServiceId(args.serviceId)
+										.setEventId(event.id).setHandleSimilarEvents(true).build();
+
+								start = Instant.now(); // timer
+
+								Response<EmptyResult> hideResponse = apiClient.post(hideRequest);
+
+								finish = Instant.now(); // timer
+								timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+								overopsTimer.add(timeElapsed);
+
+								if (hideResponse.isBadResponse())
+									throw new IllegalStateException("Failed hiding event " + event.id);
+							}
+
+						} else {
+							// 3. anything else in Jira → move to inbox in OO
+							System.out.print(" OTHER in Jira");
+
+							// if not in inbox, move to inbox in OverOps
+							if (!isInbox) {
+								System.out.print(" NOT in Inbox in OverOps. Moving to Inbox...");
+								EventInboxRequest inboxRequest = EventInboxRequest.newBuilder().setServiceId(args.serviceId)
+									.setEventId(event.id).setHandleSimilarEvents(true).build();
+
+								start = Instant.now();
+
+								Response<EmptyResult> resolvedResponse = apiClient.post(inboxRequest);
+
+								finish = Instant.now(); // timer
+								timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+								overopsTimer.add(timeElapsed);
+
+								if (resolvedResponse.isBadResponse())
+									throw new IllegalStateException("Failed moving event to inbox " + event.id);
+							}
+
 						}
 
 					}
@@ -306,22 +382,30 @@ public class JiraIntegrationFunction {
 				}
 
 				// make logs easier to read
-				System.out.println(" ");
+				System.out.println("");
 			}
 
 			// make logs easier to read
-			System.out.println(" ");
+			System.out.println("");
+
+			// compute average response times
+			double jiraTotal = 0;
+			for (int i = 0; i < jiraTimer.size(); i++) {
+				jiraTotal += jiraTimer.get(i);
+			}
+			System.out.println("AVERAGE JIRA RESPONSE TIME: " + (jiraTotal / jiraTimer.size()));
+			System.out.println("TOTAL JIRA TRANSACTIONS: " + jiraTimer.size());
+
+			double overopsTotal = 0;
+			for (int i = 0; i < overopsTimer.size(); i++) {
+				overopsTotal += overopsTimer.get(i);
+			}
+			System.out.println("AVERAGE OVEROPS RESPONSE TIME: " + (overopsTotal / overopsTimer.size()));
+			System.out.println("TOTAL OVEROPS TRANSACTIONS: " + overopsTimer.size());
 
 		} catch (Exception e) {
 			System.out.println("Caught exception. Check settings and try again.");
-
-			// https://stackoverflow.com/questions/1149703/how-can-i-convert-a-stack-trace-to-a-string
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			e.printStackTrace(pw);
-			String sStackTrace = sw.toString(); // stack trace as a string
-			System.out.println(sStackTrace);
-
+			e.toString();
 			System.exit(1);
 		}
 
@@ -362,8 +446,8 @@ public class JiraIntegrationFunction {
 			Action a = actions.get(0);
 			overopsDate = new DateTime(a.timestamp);
 		} else {
-			// if null, use now
-			overopsDate = new DateTime();
+			// if null, use epoch
+			overopsDate = new DateTime("1970-01-01T00:00:00");
 		}
 
 		return overopsDate;
@@ -412,10 +496,10 @@ public class JiraIntegrationFunction {
 		public String hiddenStatus;
 		public String resurfacedStatus;
 
-		// TODO
-		// public boolean handleSimilarEvents;
-		// public boolean updateJira;
-		// public boolean updateOverOps;
+		// public boolean updateJira;---> FALSE BY DEFAULT
+    // -----> by default this is a ONE WAY SYNC ONLY: Jira to OverOps, not OverOps to Jira.
+    // -----> look into event actions to see if there's user, at a minimum, update the ticket to say "close by OverOps" 
+    // for one way sync, Jira is king (ignore date)
 
 		private JiraIntegrationInput(String raw) {
 			super(raw);
@@ -450,7 +534,7 @@ public class JiraIntegrationFunction {
 		contextArgs.serviceId = "S38358";
 
 		// Use "Jira UDF" View
-		SummarizedView view = ViewUtil.getServiceViewByName(contextArgs.apiClient(), contextArgs.serviceId, "Jira UDF");
+		SummarizedView view = ViewUtil.getServiceViewByName(contextArgs.apiClient(), contextArgs.serviceId, "All Events");
 		contextArgs.viewId = view.id;
 
 		String rawContextArgs = new Gson().toJson(contextArgs);
